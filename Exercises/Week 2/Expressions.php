827b4aeba21522c7867e5a7ace42c65c3c7120ae
nam98nd16 <nam98nd16@gmail.com>
2020-03-10 09:57:52 +0700

    add week 2 class example

---
+++ Exercises/Week 2/Expressions.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+            Expression Example
+        </title>
+    </head>
+    <body>
+        <?php
+        $gr1 = 123;
+        $gr2 = 14;
+        $gr3 = 5435;
+        $aver = ($gr1 + $gr2 + $gr3) / 3 ;
+        print "Average is $aver";
+        ?>
+    </body>
+</html>
